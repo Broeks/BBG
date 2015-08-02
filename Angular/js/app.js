1a214d0931d3c3ef9fb4249979e70d7632f8e0bf
salpalvv <gth701m@gmail.com>
2015-08-02 18:58:07 -0400

    completed second porition of the Angular class so just updating the files with that

--- Angular/js/app.js
+++ Angular/js/app.js
@@ -5,18 +5,42 @@
 		this.products = gems;
 	});
 
+	app.controller('PanelController', function(){
+		this.tab = 1;
+
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
+
 	var gems = [
-		{
-			name: 'Dodecahedron',
-			price: 2.95,
-			description: '. . .',
-			canPurchase: true,
+		{ 	
+			name: 'Dodecahedron', 
+			price: 2, 
+			description: '12 sides', 
+			canPurchase: true, 
+			images: [
+				{
+					full: 'img/dog1.jpg',
+					thumb: 'img/dog2.jpg'
+				}
+			]
 		},
-		{
-			name: 'Pentagonal Gem',
-			price: 5.95,
-			description: '. . .',
-			canPurchase: false,
+		{ 
+			name: 'Pentagonal Gem', 
+			price: 5.95, 
+			description: '5 sides', 
+			canPurchase: false, 
+			images: [
+				{
+					full: 'img/dog3.jpg',
+					thumb: 'img/dog4.jpg'
+				}
+			]
 		}
 	];
 })();
